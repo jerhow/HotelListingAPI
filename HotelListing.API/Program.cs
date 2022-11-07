e46d4730b9f607f75584bb2df8aedae2b6e95bc1
Jerry Howard <jerryhoward@fastmail.fm>
2022-11-06 17:32:22 -0800

    Configured logging through Serilog, with Seq as the log aggregator. Three NuGet packages were also installed: - Serilog.AspNetCore - Serilog.Expressions - Serilog.Sinks.Seq

--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,6 +15,8 @@ builder.Services.AddCors(options =>
                                         .AllowAnyOrigin()
                                         .AllowAnyMethod());
 });
+
+builder.Host.UseSerilog((context, loggerConfiguration) => loggerConfiguration.WriteTo.Console().ReadFrom.Configuration(context.Configuration));
 
 var app = builder.Build();
 
