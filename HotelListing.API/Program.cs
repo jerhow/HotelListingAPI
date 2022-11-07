4d9b2794f75aac11dae863bb39836a332d9601fb
Jerry Howard <jerryhoward@fastmail.fm>
2022-11-06 18:31:28 -0800

    Sets up Entity Framework for SQL Server, connection string (in appsettings, which I'm only doing here to comply with the course i am taking), and a DBContext class for connecting

--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -1,8 +1,16 @@
+using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connStr = builder.Configuration.GetConnectionString("HotelListingDbConnStr");
+builder.Services.AddDbContext<HotelListingDbContext>(options =>
+{
+    options.UseSqlServer(connStr);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
