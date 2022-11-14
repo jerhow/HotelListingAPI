c78f2ba2aeca8328c6afbdbb162ea54f36c7a47d
Jerry Howard <jerryhoward@fastmail.fm>
2022-11-14 13:40:47 -0800

    Added a DTO for Country creation (the POST controller action), which basically gives us a type of 'view model' that we can expose to the client with only the appropriate fields, provide abstraction around our internal model, and impose conditions on the creation of new Country objects (required fields, etc)

---
+++ HotelListing.API/DTO/Country/CreateCountryDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.API.DTO.Country
+{
+    public class CreateCountryDto
+    {
+        [Required]
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+    }
+}
